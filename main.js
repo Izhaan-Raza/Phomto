034063499400f88158ddaf0407cdeba82edb9f9f
Izhaan Raza <izzupro3232@gmail.com>
2023-07-25 16:28:48 +0530

    kjdhajk

---
+++ main.js
@@ -0,0 +1,9 @@
+// Get the reference to the image element
+const image = document.getElementById("imm");
+
+// Add a click event listener to the image
+image.addEventListener("click", function () {
+  // Your code for the click function goes here
+  // For example, you can open a link or perform any action you desire
+  console.log("Image clicked!");
+});
